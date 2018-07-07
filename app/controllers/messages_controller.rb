b1f735c87e1c91f8485da8256a704698d77db131
h-kmt <puff.h.kmt@gmail.com>
2018-07-07 17:02:42 +0900

    footerのfomeを追加
    とりあえず実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to new_user_path
+  end
+
+  private
+
+   def user_params
+     params.require(:user).permit(:text)
+     params.require(:user).permit(:image)
+   end
 end
