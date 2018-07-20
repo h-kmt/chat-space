4454b9a78dc832d793d685cd757006f7f5c525dc
h-kmt <puff.h.kmt@gmail.com>
2018-07-20 17:14:06 +0900

    日本語表示エラー

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = :ja
+    I18n.enforce_available_locales = true
+    config.active_record.default_timezone = :local
   end
 end
 
